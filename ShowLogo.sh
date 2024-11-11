5f4108fa320459186eaae27374b8f0ea21b0981d
NODEBOY <kingindoslayer@gmail.com>
2024-11-12 01:55:46 +0700

    Update ShowLogo.sh

--- ShowLogo.sh
+++ ShowLogo.sh
@@ -10,10 +10,8 @@ echo -e "\e[1m
 ██║ ╚████║╚██████╔╝██║     ██║  ██║██║ ╚████║    ██║  ██║██║  ██║██║ ╚═╝ ██║██████╔╝███████╗
 ╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═══╝    ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝╚═════╝ ╚══════╝
                                                                                             
-
 \e[0m"
 echo -e "\e[1mBLOCKMESH NETWORK\e[0m";
-echo -e "\n
 echo -e "\e[1mWelcome & Enjoy sir!\e[0m";
 echo -e "\e[1mAuthor : \e[1mNofan Rambe\e[0m";
 echo -e ""
